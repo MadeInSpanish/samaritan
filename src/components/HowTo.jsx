edc2a3340b2eefa5df61cbe790fc2b7ad8489b45
Alex <alexlopezarroyave@gmail.com>
2017-02-28 16:46:04 -0500

    how it works

--- src/components/HowTo.jsx
+++ src/components/HowTo.jsx
@@ -19,7 +19,13 @@ export default class HowTo extends Component {
 
     return (
       <div className={howtoClasses}>
-        <button className="howtop__button" onClick={this.handleHowtoClick}>How it works</button>
+        <button className="howtop__button" onClick={this.handleHowtoClick}>HOW IT WORKS</button>
+        <ul className="howtop__list">
+          <li className="howtop__item">1.COPY/paste <span>UR</span> post URL</li>
+          <li className="howtop__item intro">2.press<span>-</span>INTRO</li>
+          <li className="howtop__item">3.DOWNLOAD <span>images</span></li>
+          <li className="howtop__item enjoy">& ENJOY</li>
+        </ul>
       </div>
     )
   }
