38c9fb6028d33380e9f005b7a4bff5990786fe1c
Alex <alexlopezarroyave@gmail.com>
2017-02-28 01:22:29 -0500

    hardcoded scraper

---
+++ micro.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+
+app.use(cors());
+
+
+
+
+const querystring = require('querystring');
+const keyword_extractor = require("keyword-extractor");
+const Xray = require('x-ray')
+const natural = require('natural')
+
+const word2rep = '(adsbygoogle = window.adsbygoogle || []).push({});'
+const xray = new Xray({
+  filters: {
+    trim: val => typeof val === 'string' ? val.replace(/(\r\n|\n|\r)/gm,"") : value,
+    removeAnalytics: val => typeof val === 'string' ? val.replace(word2rep, "") : value,
+  }
+})
+
+
+app.get('/', function(req, res, next){
+  const web = querystring.parse( req.url.replace('/?', '') ) || { web: '' }
+
+  if ('web' in web) {
+    xray(web.web, {
+      title: 'title',
+      article: 'article .entry-content | trim | removeAnalytics'
+    })((err, obj) => {
+      const save = []
+      const tfidf = new natural.TfIdf()
+      const data = keyword_extractor.extract(obj.article,{
+        language: "spanish",
+        remove_digits: true,
+        return_changed_case: true,
+        remove_duplicates: true,
+      });
+
+      tfidf.addDocument(data)
+      tfidf.listTerms(0).forEach((item, index) => {
+        if (item.tfidf > 0 && save.length < 10 && item.term.length > 3) {
+          save.push(item.term)
+        }
+      })
+
+
+      res.json({ words: save });
+    })
+  }
+
+});
+
+app.listen(3001, function(){
+ console.log('CORS-enabled web server listening on port 3001');
+});
