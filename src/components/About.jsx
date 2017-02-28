5c5db2808fe33d1ce00ba8049e9a4fb8938d551b
Ana Maria Sosa <anamariasosam@gmail.com>
2017-02-28 09:18:35 -0500

    Add new components: about, howto, contact and netorks

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div className="AboutP">
+        <h5 className="AboutP__deck">
+          <b>Samaritan</b>
+        </h5>
+      </div>
+    )
+  }
+}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,7 +16,7 @@ export default class Home extends Component {
 
 
         <div className="homeP__howtoContainer">
-          <Link to="/how-to" className="homeP__howto">How it works</Link>
+          <Link to="/how-to/" className="homeP__howto">How it works</Link>
         </div>
       </div>
     )
